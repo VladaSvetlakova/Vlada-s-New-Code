9f43e4ae5276a8d4637b6f750e59fed1f4f5b873
Vlada Svetlakova <vladas3@hotmail.com>
2020-02-06 17:13:06 -0700

    practicing git

---
+++ Another.js
@@ -0,0 +1,14 @@
+function addOne(number){
+     increased = number+1;
+     console.log(increased);
+}
+addOne(4);
+
+array = [1,2,3,4];
+
+array.forEach(function(number){
+    added = number+3;
+    console.log(added);
+
+
+});
